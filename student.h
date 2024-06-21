f1e6c6bb38f922d2ac357faea4d8c570fac3f138
RronKabashi <120381587+RronKabashi@users.noreply.github.com>
2024-06-21 10:58:41 +0200

    Update student.h

--- student.h
+++ student.h
@@ -2,15 +2,25 @@
 #define STUDENT_H
 
 #define NAME_LENGTH 50
+#define COURSE_NAME_LENGTH 50
+
+typedef struct {
+    int courseId;
+    char courseName[COURSE_NAME_LENGTH];
+} Course;
 
 typedef struct {
     int id;
     char name[NAME_LENGTH];
+    Course* courses;
+    int courseCount;
+    int courseCapacity;
 } Student;
 
 void addStudent(int id, const char* name);
 void deleteStudent(int id);
 void searchStudent(int id);
 void displayAllStudents(void);
+void addCourseToStudent(int studentId, int courseId, const char* courseName);
 
 #endif // STUDENT_H
