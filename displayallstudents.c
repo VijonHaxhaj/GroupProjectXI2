a2bb7165f02b5a7baa27378716b2d79032927198
blendikuleta <165767934+blendikuleta@users.noreply.github.com>
2024-06-21 11:49:04 +0200

    Update displayallstudents.c
    pjesa e adriatikut per det 2 a2

--- displayallstudents.c
+++ displayallstudents.c
@@ -14,3 +14,12 @@ typedef struct StudentList {
     int count;
     int capacity;
 } StudentList;
+// Function to display all student details
+void displayAllStudents(const StudentList* list) {
+    printf("Student List:\n");
+    printf("ID\tName\t\tAge\n");
+    printf("-----------------------------\n");
+    for (int i = 0; i < list->count; i++) {
+        printf("%d\t%-15s\t%d\n", list->students[i].id, list->students[i].name, list->students[i].age);
+    }
+}
